cf6cdce894671e1572eba527b715a409e672130e
aschneider15 <aschneider15@outlook.com>
2022-11-17 22:08:30 -0600

    Script Categorization
    All entities' scripts have been placed in the "Scripts\Entities" folder. Will be working on a "Scripts\DataPersistence" folder when I am tinkering with the save system.

---
+++ My project/Assets/Scripts/Entities/PlayerController.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 movementInput;
+    Rigidbody2D rb;
+    public float MoveSpeed = 1F;
+    public float collisionOffset = 0.05F;
+    public ContactFilter2D movementfilter;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    Animator animator;
+    SpriteRenderer SpriteRenderer;
+    int health = 100;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+        Debug.Log("Player health: " + health);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+
+        if (movementInput != Vector2.zero)
+        {
+            bool success = TryMove(movementInput);
+            if (!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+            }
+            animator.SetBool("IsMoving", success);
+        }
+        else
+        {
+            animator.SetBool("IsMoving", false);
+        }
+        if (movementInput.x < 0)
+        {
+            SpriteRenderer.flipX = true;
+        }
+        else if (movementInput.x > 0)
+        {
+            SpriteRenderer.flipX = false;
+        }
+
+
+
+    }
+    private bool TryMove(Vector2 direction)
+    {
+        int count = rb.Cast(movementInput, movementfilter, castCollisions, MoveSpeed * Time.fixedDeltaTime + collisionOffset);
+        if (count == 0)
+        {
+            rb.MovePosition(rb.position + movementInput * MoveSpeed * Time.fixedDeltaTime);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
+    }
+   
+    void OnFire()
+    {
+        animator.SetTrigger("SwingSword");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            Debug.Log("TESTING");
+        }
+    }
+
+}
