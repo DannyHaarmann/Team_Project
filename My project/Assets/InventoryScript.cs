3beb42b7ba043189467cdf3516bcee56b62cdd4e
hunterfihn <101016842+hunterfihn@users.noreply.github.com>
2022-12-02 11:59:43 -0600

    StartInventory

---
+++ My project/Assets/InventoryScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool InventoryIsOpen = false;
+    public GameObject InventoryMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            if(InventoryIsOpen)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        InventoryMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        InventoryIsOpen = false;
+    }
+
+    void Pause()
+    {
+        InventoryMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        InventoryIsOpen = true;
+    }
+
+}
